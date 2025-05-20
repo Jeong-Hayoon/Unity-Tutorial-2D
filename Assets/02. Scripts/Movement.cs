ae1fe7d06978dfd0a788b78dc778b9542324cf97
Jeong-Hayoon <137061867+Jeong-Hayoon@users.noreply.github.com>
2025-05-20 11:34:39 +0900

    [Add] Unity Project
    - Asset
    - Packages
    - ProjectSettings

---
+++ Assets/02. Scripts/Movement.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public float moveSpeed;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        // 앞으로 가는 기능
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.position += Vector3.forward * moveSpeed * Time.deltaTime;
+        }
+
+        // 뒤로 가는 기능
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.position += Vector3.back * moveSpeed * Time.deltaTime;
+        }
+
+        // 왼쪽으로 가는 기능
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.position += Vector3.left * moveSpeed * Time.deltaTime;
+        }
+
+        // 오른쪽으로 가는 기능
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.position += Vector3.right * moveSpeed * Time.deltaTime;
+        }
+    }
+}
